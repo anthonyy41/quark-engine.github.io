0b9069e34fe10864e57cfa238fe1a3baf5cc2a1b
Shiang <pulorsok@gmail.com>
2020-06-04 17:33:32 +0800

    update

--- js/collapse/script.js
+++ js/collapse/script.js
@@ -7,8 +7,51 @@ $(document).ready(function() {
     .on('hide.bs.collapse', function(a) {
       $(a.target).prev('.panel-heading').removeClass('active');
     });
+  $(".close").click(function() {
+    if ($(this).hasClass("active")){
+      $(this).removeClass("active")
+      $(".panel-collapse").collapse('hide')
+    }else{
+      $(this).addClass("active")
+      $(".panel-collapse").collapse('show')
+    }
     
-	
-});
+  })
 
+  $('#confidence').change(function(){
+    var filter_value = $(this).find('option:selected').val()
+    var panel_id = ".panel" + filter_value
+
+    $(".panel20").hide()
+    $(".panel40").hide()
+    $(".panel60").hide()
+    $(".panel80").hide()
+    $(".panel100").hide()
+
+    if (filter_value == 20){
+      $(panel_id).show()
+    } else if (filter_value == 40){
+      $(panel_id).show()
+    } else if (filter_value == 60){
+      $(panel_id).show()
+    } else if (filter_value == 80){
+      $(panel_id).show()
+    } else if (filter_value == 100){
+      $(panel_id).show()
+    } else if (filter_value == "all"){
+      $(".panel20").show()
+      $(".panel40").show()
+      $(".panel60").show()
+      $(".panel80").show()
+      $(".panel100").show()
+    } else if (filter_value == "confidence"){
+      $(".panel20").show()
+      $(".panel40").show()
+      $(".panel60").show()
+      $(".panel80").show()
+      $(".panel100").show()
+    }
+  })
+  
+});
 
